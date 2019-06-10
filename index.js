d6abf911d0e5685d9350e8d14e888dd484c8b630
Daniel Schneider <daschne8@gmail.com>
2019-06-10 16:05:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = "CRASH"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + '!'
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
